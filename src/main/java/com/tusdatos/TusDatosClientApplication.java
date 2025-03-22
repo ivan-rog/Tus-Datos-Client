05cc1fcc2d41587cd2fc9b518eaebe03d2ad802a
ivan-rog <ivan.romero.develop@gmail.com>
2025-03-22 15:02:22 -0600

    Feat: Initial project structure

---
+++ src/main/java/com/tusdatos/TusDatosClientApplication.java
@@ -0,0 +1,13 @@
+package com.tusdatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusDatosClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusDatosClientApplication.class, args);
+    }
+
+}
