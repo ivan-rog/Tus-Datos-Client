8dd85581e42c45703e3eaac7af06992eb2a0f9db
ivan-rog <ivan.romero.develop@gmail.com>
2025-03-23 12:20:19 -0600

    feat: Add by default the Content Type header

--- src/main/java/com/tusdatos/configuration/WebClientConfig.java
+++ src/main/java/com/tusdatos/configuration/WebClientConfig.java
@@ -6,6 +6,8 @@ import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -42,16 +44,16 @@ public class WebClientConfig {
 
     @Bean
     public WebClient createWebClient() {
-        return WebClient.
-                builder().
-                filters(
+        return WebClient.builder()
+                .filters(
                         exchangeFilterFunctions -> {
                             exchangeFilterFunctions.add(this.logRequest());
                             exchangeFilterFunctions.add(this.logResponse());
                         }
-                ).
-                clientConnector(new ReactorClientHttpConnector(createHttpClient())).
-                build();
+                )
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .clientConnector(new ReactorClientHttpConnector(createHttpClient()))
+                .build();
     }
 
     private ExchangeFilterFunction logRequest() {
